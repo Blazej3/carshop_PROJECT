a52ee06cf0ca9a5e1b849f8fa619f8f35fc02b12
Blazej3 <b.goszczyn88@gmail.com>
2024-03-31 19:08:54 +0300

    added add+edit functionality

--- src/components/Carlist.jsx
+++ src/components/Carlist.jsx
@@ -3,8 +3,8 @@ import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-material.css"; // Optional Theme applied to the grid
 import Button from '@mui/material/Button';
-
-
+import Addcar from './Addcar';
+import Editcar from './Editcar'
 
 function Carlist() {
 
@@ -12,31 +12,39 @@ function Carlist() {
 
 
     const [colDefs] = useState([
-        { field: "brand", filter: true },
-        { field: "model", filter: true },
-        { field: "color", filter: true },
+        { field: "brand", filter: true, width: 150 },
+        { field: "model", filter: true, width: 100 },
+        { field: "color", filter: true, width: 100 },
         { field: "fuel", filter: true, width: 100 },
         { field: "modelYear", filter: true, width: 100 },
-        { field: "price", filter: true },
+        { field: "price", filter: true, width: 100 },
         {
-            cellRenderer: params => <Button size="small" color="error" onClick={() => deleteCar(params.data._links.car.href)}>
-                Delete
-            </Button>
+            width: 100,
+            cellRenderer: params => <Editcar updateCar={updateCar} car={params.data} />
         },
+        {
+            cellRenderer: params =>
+                <Button size="small" color="error" onClick={() => deleteCar(params.data._links.car.href)}>
+                    Delete
+                </Button>
+        },
+
+
     ]);
 
     const deleteCar = (url) => {
-        if(window.confirm("are you sure?")){
-        fetch(url, { method: 'DELETE' })
-            .then(response => {
-                if (!response.ok)
-                    throw new Error("Error in deletion: " + response.statusText);
+        if (window.confirm("are you sure?")) {
+            fetch(url, { method: 'DELETE' })
+                .then(response => {
+                    if (!response.ok)
+                        throw new Error("Error in deletion: " + response.statusText);
 
-                return response.json();
-            })
-            .then(() => fetchCars())
-            .catch(err => console.error(err))
-    }};
+                    return response.json();
+                })
+                .then(() => fetchCars())
+                .catch(err => console.error(err))
+        }
+    };
 
 
 
@@ -58,14 +66,33 @@ function Carlist() {
 
     }
 
+    const saveCar = (car) => {
+        fetch('https://carrestservice-carshop.rahtiapp.fi/cars', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(car)
+        })
+            .then(() => fetchCars())
+            .catch(err => console.error(err))
+    }
+
+    const updateCar = (car, link) => {
+        fetch(link, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(car)
+        })
+            .then(() => fetchCars())
+            .catch(err => console.error(err))
+
+    }
+
 
 
     return (
-        // wrapping container with theme & size
-        <div
-            className="ag-theme-material" // applying the grid theme
-            style={{ height: 600 }} // the grid will fill the size of the parent container
-        >
+
+        <div className="ag-theme-material" style={{ height: 600 }}>
+            <Addcar saveCar={saveCar} />
             <AgGridReact
                 rowData={cars}
                 columnDefs={colDefs}
